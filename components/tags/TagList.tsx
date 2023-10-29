eff7153aeb9a78b33b2b19bdb892acb1005ff85e
hyeon9782 <hyeon9782@gmail.com>
2023-10-29 21:12:23 +0900

    fix: build error 해결 2

--- components/tags/TagList.tsx
+++ components/tags/TagList.tsx
@@ -5,15 +5,18 @@ import useCurrentTab from '@/stores/useCurrentTab';
 
 type Props = {
   tags: string[];
+  onClick?: (tag: string) => void;
 };
-const TagList = ({ tags }: Props) => {
+const TagList = ({ tags, onClick }: Props) => {
   const { setTab } = useCurrentTab();
 
   const handleTagClick = (tag: string) => {
     setTab(tag);
   };
   return (
-    <ul className={tagList}>{tags?.map((tag, index) => <Tag key={index} tag={tag} onTagClick={handleTagClick} />)}</ul>
+    <ul className={tagList}>
+      {tags?.map((tag, index) => <Tag key={index} tag={tag} onTagClick={onClick ? onClick : handleTagClick} />)}
+    </ul>
   );
 };
 
