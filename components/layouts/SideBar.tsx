ad67a9b34c3a6fb660557875d83a4886318e1015
hyeon9782 <hyeon9782@gmail.com>
2023-10-29 21:05:45 +0900

    refactor: SideBar 서버 컴포넌트로 변경

--- components/layouts/SideBar.tsx
+++ components/layouts/SideBar.tsx
@@ -1,29 +1,16 @@
-'use client';
 import TagList from '../tags/TagList';
 import { sideBar, sideBarText } from '@/styles/layout.css';
 import { sidePadding } from '@/styles/common.css';
-import useCurrentTab from '@/stores/useCurrentTab';
-import { useQuery } from '@tanstack/react-query';
 import { getTags } from '@/services/tags';
 
-const SideBar = () => {
-  const { data: tags } = useQuery({
-    queryKey: ['tags'],
-    queryFn: getTags,
-    select: res => res.tags,
-  });
-
-  const { setTab } = useCurrentTab();
-
-  const handleTagClick = (tag: string) => {
-    setTab(tag);
-  };
+const SideBar = async () => {
+  const tags = await getTags().then(res => res.tags);
 
   return (
     <div className={sidePadding}>
       <article className={sideBar}>
         <p className={sideBarText}>Popular Tags</p>
-        <TagList tags={tags} onClick={handleTagClick} />
+        <TagList tags={tags} />
       </article>
     </div>
   );
--- components/tags/TagList.tsx
+++ components/tags/TagList.tsx
@@ -1,12 +1,20 @@
+'use client';
 import { tagList } from '@/styles/layout.css';
 import Tag from './Tag';
+import useCurrentTab from '@/stores/useCurrentTab';
 
 type Props = {
   tags: string[];
-  onClick?: (tag: string) => void;
 };
-const TagList = ({ tags, onClick }: Props) => {
-  return <ul className={tagList}>{tags?.map((tag, index) => <Tag key={index} tag={tag} onTagClick={onClick} />)}</ul>;
+const TagList = ({ tags }: Props) => {
+  const { setTab } = useCurrentTab();
+
+  const handleTagClick = (tag: string) => {
+    setTab(tag);
+  };
+  return (
+    <ul className={tagList}>{tags?.map((tag, index) => <Tag key={index} tag={tag} onTagClick={handleTagClick} />)}</ul>
+  );
 };
 
 export default TagList;
